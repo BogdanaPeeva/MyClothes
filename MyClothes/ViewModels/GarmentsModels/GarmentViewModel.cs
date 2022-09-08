63300e244eaa93532e6cbef33cdf5132b5913a72
SONY <SONY@DESKTOP-0TOU3E0>
2022-09-08 14:00:04 +0300

    Add project files.

---
+++ MyClothes/ViewModels/GarmentsModels/GarmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using MyClothes.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClothes.ViewModels.GarmentsModels
+{
+    public class GarmentViewModel
+    {
+        public string GarmentId { get; set; }
+        public string ImageUrl { get; set; }
+        public string? CategoryName { get; set; }
+        public string? SeasonName { get; set; }
+        public string? ColourName { get; set; }
+
+        public string Description { get => $"Category: {this.CategoryName ?? "none"} <br> Season: {this.SeasonName ?? "none"} <br> Colour: {this.ColourName ?? "none"}"; }
+
+
+
+    }
+}
